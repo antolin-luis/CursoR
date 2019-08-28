4ed92fe15d252ffe3f4c0b028498996568f76f79
Luís Antolin <antolin.luis@gmail.com>
2019-08-28 18:09:33 -0300

    Add files via upload

--- AulaR.R
+++ AulaR.R
@@ -14,7 +14,9 @@ install.packages("C:/caminho/nome-do-pacote.zip", repos = NULL).
 # CARREGANDO APENAS UM PACOTE
 library(nome do pacote)
 
-## 2. OPERADORES NUMÉRICOS EM R ----
+## 2. OPERADORES EM R ----
+
+# 2.1 - OPERADORES NUMÉRICOS
 
 1 + 1    # adição
 ## [1] 2
@@ -30,6 +32,65 @@ library(nome do pacote)
 ## [1] 2
 5 %/% 3  # parte inteira da divisão de 5 por 3
 ## [1] 1
+
+
+# 2.2 - OPERADORES MATEMÁTICOS COM O R
+
+sqrt(9)   # Raiz Quadrada
+#[1] 3
+abs( - 1 )  # Módulo ou valor absoluto
+#[1] 1
+abs( 1 )
+#[1] 1
+log( 10 )   # Logaritmo natural ou neperiano (ln)
+#[1] 2.302585
+log( 10, base = 10) # Logaritmo base 10
+#[1] 1
+log10(10)   # Também logaritmo de base 10
+#[1] 1
+log( 10, base = 3.4076) # Logaritmo base 3.4076
+#[1] 1.878116
+exp( 1 )       # Exponencial
+#[1] 2.718282
+factorial( 4 )         # Fatorial de 4
+#[1] 24
+choose(10, 3)          # Coeficientes binomiais: combinação de 10 3-a-3
+#[1] 120
+
+
+# 2.3 - FUNÇÕES TRIGONOMÉTRICAS
+
+sin(0.5*pi)    # Seno
+#[1] 1
+cos(2*pi)      # Coseno
+#[1] 1
+tan(pi)        # Tangente
+#[1] -1.224647e-16
+asin(1)       # Arco seno (em radianos)
+#[1] 1.570796
+asin(1) / pi * 180
+#[1] 90
+acos(0)      # Arco coseno (em radianos)
+#[1] 1.570796
+acos(0) / pi * 180
+#[1] 90
+atan(0)    # Arco tangente (em radianos)
+#[1] 0
+atan(0) / pi * 180
+#[1] 0
+
+# 2.4 - FUNÇÕES DE ARREDONDAMENTO
+
+ceiling( 4.3478 )
+#[1] 5
+floor( 4.3478 )
+#[1] 4
+round( 4.3478 )
+#[1] 4
+round( 4.3478 , digits=3)
+#[1] 4.348
+round( 4.3478 , digits=2)
+#[1] 4.35
 
 
 ## 3. TIPOS DE OBJETO ----
@@ -112,8 +173,77 @@ y <- c(1.7, "a")  ## character
 
 #character > complex > numeric > integer > logical
 
+# 3.3 - SEQUÊNCIAS NUMÉRICAS
+
+# SEQUÊNCIA SIMPLES
+
+1:4 # CRESCENTE
+
+4:1 # DECRESCENTE
+
+# SEQUÊNCIA COM ORDEM DEFINIDA
+
+seq(1,20,by=2) # CRESCENTE
+ 
+seq(20,1,by=-2) # DECRESCENTE
+
+seq(1,20,length=5) # QUANDO APENAS QUEREMOS UM VETOR DE TAMANHO DEFINIDO, EMBORA NÃO SAIBAMOS O ESPAÇO ENTRE OS NÚMEROS
+
+# POR REPETIÇÃO
+
+rep(3,times=5)
+
+rep(1:5, 3)
+
+rep(1:5,each=3)
+
+# SEQUÊNCIA DE NÚMEROS EM UMA DISTRIBUIÇÃO NORMAL
+
+vn1 <-  rnorm( 1000, mean = 40,  sd = 9 )
+
+# OUTRAS DISTRIBUIÇÕES:
+# rpois, rbinom, runif....
+
+# ORDENANDO A SEQUÊNCIA:
+
+x  <-  sort( rnorm(1000, mean=1) ) # CRESCENTE
+
+x  <-  sort( rnorm(1000, mean=1), decreasing = T ) # DECRECENTE
+x  <-  rev(sort( rnorm(1000, mean=1) )) # DECRESCENTE
+
+# MATEMÁTICA COM VETORES
+
+a <-  c(1, 10, 3.4, pi, pi/4, exp(-1), log( 2.23 ), sin(pi/7) )
+b <-  1:8
+c <-  20:32
+d <-  2.5:10
+ 
+sqrt( a )# RAIZ QUADRADA DE UM VETOR
+
+a - b # SOMA OU SUBTRAÇÃO DE VETORES
+
+a^(1/b) # POTENCIA DE VETORES
+
+mean( a ) # MÉDIA
+
+var( b ) # VARIÂNCIA
+
+max( c ) # VALOR MÁXIMO DENTRO DO VETOR
+
+min( b ) # VALOR MÍNIMO DENTRO DO VETOR
+
+sd( a ) # DESVIO PADRÃO
+
+sum( c ) # SOMA DOS VALORES DENTRO DE UM VETOR
+
+range( c ) # EXTREMOS MÍNIMOS E MÁXIMOS DENTRO DE UM VETOR
+
+cumsum(a) # SOMATÓRIO ACUMULATIVO DOS ELEMENTOS DO VETOR
+
+diff(a) # DIFERENÇA ENTRE OS ELEMENTOS DO VETOR
+
 # 3.3 - MATRIZES
-# S?O VETORES DE 2 DIMENS?ES QUE ACEITAM APENAS ELEMENTOS DE UMA CLASSE
+# SÃO VETORES DE 2 DIMENSÕES QUE ACEITAM APENAS ELEMENTOS DE UMA CLASSE
 
 m <- matrix(1:6, nrow = 2, ncol = 3)
 m
@@ -122,8 +252,18 @@ m[3,  ]   # seleciona a terceira linha
 m[ , 2]   # seleciona a segunda coluna
 m[1, 2]   # seleciona o primeiro elemento da segunda coluna
 t(m)      # matriz transposta
-m %*% n   # multiplica??o matricial
+m %*% n   # multiplicação matricial
 solve(m)  # matriz inversa de m
+
+# CRIANDO UMA MATRIZ ALEATÓRIA (INCIDÊNCIA DE UMA DOENÇA POR TRATAMENTO)
+m1 <- matrix( 
+ rnorm(20,50,10), 
+  ncol = 2,
+  nrow = 10,
+  byrow = TRUE)
+
+colnames(m1) <- c('Tratamento 1', 'Tratamento 2')
+rownames(m1) <- 1:10
 
 # 3.4 - LISTAS
 
@@ -131,13 +271,13 @@ x <- list(1:5, "Z", TRUE, c("a", "b"))
 x
 
 # 3.5 - DATA.FRAME
-# S?O TABELAS COM DADOS A SEREM TRABALHADOS, CONTENDO ELEMENTOS DE MULTIPLAS CLASSES
+# SÃO TABELAS COM DADOS A SEREM TRABALHADOS, CONTENDO ELEMENTOS DE MULTIPLAS CLASSES
 
 # 4 - IMPORTANDANDO DADOS ----
 
 tb1 <- read.csv('piraclim.csv',dec = ',')
 
-#COMANDOS SUPER ÚTEIS
+# 4.1 - COMANDOS SUPER ÚTEIS
 
 head(tb1) # Mostra as primeiras 6 linhas.
 tail(tb1) # Mostra as últimas 6 linhas.
@@ -147,59 +287,68 @@ dim(tb1) # Número de linhas e de colunas.
 names(tb1) # Os nomes das colunas (variáveis).
 str(tb1) # Estrutura do data.frame. Mostra, entre outras coisas, as classes de cada coluna.
 
+# 4.2 - MUDANÇA DE CLASSES DE UMA COLUNA:
+  #CASO ESPECIAL PARA FATORES:
+
+
+
+
 
 # 5 - TRABALHANDO EM UM BANCO DE DADOS ----
 
-# 5.1 - FILTRANDO DADOS POR UMA VARIÁVEL
-tb2 <- tb2[tb2$NOME!=' ',]
-# 5.2 - EXECUTANDO OPERAÇÕES MATEMÁTICAS
+# 5.1 OPERADORES LÓGICOS
+
+x == y # x igual a y
+x != y # x diferente de y
+x < y #	x menor que y
+x <= y #	x menor ou igual a y
+x > y #	x maior que y
+x >= y # x maior ou igual a y
+!x 	# Negativa de x
+x | y #	x ou y são verdadeiros
+x & y #	x e y são verdadeiros
+xor(x, y) #	x ou y são verdadeiros (apenas um deles)
+x %in% y - x em y
+
+# 5.2 - FILTRANDO DADOS POR UMA VARIÁVEL
+tb1 <- tb1[tb1$NOME!=' ',]
+# 5.3 - EXECUTANDO OPERAÇÕES MATEMÁTICAS
 # DESCOBRIR O TAMANHO DA ÁREA DE LATOSSOLO VERMELHO NO BRASIL
 unique(tb2$DESC_)
 
 # CONTINUAR A PARTIR DAQUI
 
 
-# 5.3 - UNINDO CONJUNTOS DE DADOS
+# 5.4 - UNINDO CONJUNTOS DE DADOS
 s1 <- tb1[tb1$ANO=='1986',]
 s2 <- tb1[tb1$ANO=='2018',]
 
 s3 <- rbind(s1,s2)
 
-# 5.4 - CONTROLADORES DE FLUXO 
+# 5.5 - CONTROLADORES DE FLUXO 
 
-# 5.4.1 - IF (OU ESTRUTURA CONDICIONAL)
+# 5.5.1 - IF (OU ESTRUTURA CONDICIONAL)
 
-
-# TESTE DA CONDIÇÃO COMO VERDADEIRA (TRUE) OU FALSA (FALSE)
-# x < y -	x menor que y?
-# x <= y -	x menor ou igual a y?
-# x > y -	x maior que y?
-# x >= y - x maior ou igual a y?
-# x == y - x igual a y?
-# x != y - x diferente de y?
-# !x 	Negativa de x
-# x | y -	x ou y s?o verdadeiros?
-# x & y -	x e y s?o verdadeiros?
-# xor(x, y) -	x ou y s?o verdadeiros (apenas um deles)?
-# x %in% y - x em y
-
+x <- 1991
 if(x %in% tb1$ANO){
   print('Ano presente')
+}else{
+  print('Ano não presente')
 }
 
-# CONTINUAR PARA TODAS AS TEXTURAS DE SOLO
-
-# 5.4.2 - FOR (OU ESTRUTURA DE REPETI??O)
-# SEPARANDO UM N?MERO LIMITADO DE MUNIC?PIOS
-
-# df <- c()
-# for(i in 1:nrow(tb1)){
-#   d <- tb2[tb2$GEOCODIGO==tb1$GEOCOD[i],]
-#   df <- rbind(df,d)
-# }
 
 
-# 5.5 - EXPORTANDO DADOS
+# 5.5.2 - FOR (OU ESTRUTURA DE REPETI??O)
+# SEPARANDO UM NÚMERO LIMITADO DE OCORRÊNCIAS
+
+df <- c()
+for(i in 1:nrow(tb1)){
+  d <- tb1[tb1$RAIN>=tb1$GEOCOD[i],]
+  df <- rbind(df,d)
+}
+
+
+# 5.6 - EXPORTANDO DADOS
 
 
 write.csv(df,'SoloSelect.csv',quote = F,dec=',',sep = ';',row.names = F)
