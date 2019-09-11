4d48b6e84d043c768c2b313c40ab9dfb027e95eb
Luís Antolin <antolin.luis@gmail.com>
2019-09-11 16:21:02 -0300

    Add files via upload

--- AulaR.R
+++ AulaR.R
@@ -1,4 +1,4 @@
-## 1. INSTALAÇÃO/EXECUÇÃO DE PACOTES EM R ----
+## 1. INSTALA??O/EXECU??O DE PACOTES EM R ----
 
 # 1.1 - INSTALAR PACOTE
 
@@ -16,29 +16,29 @@ library(nome do pacote)
 
 ## 2. OPERADORES EM R ----
 
-# 2.1 - OPERADORES NUMÉRICOS
+# 2.1 - OPERADORES NUM?RICOS
 
-1 + 1    # adição
+1 + 1    # adi??o
 ## [1] 2
-4 - 2    # subtração
+4 - 2    # subtra??o
 ## [1] 2
-2 * 3    # multiplicação
+2 * 3    # multiplica??o
 ## [1] 6
-5 / 3    # divisão
+5 / 3    # divis?o
 ## [1] 1.666667
-4 ^ 2    # poténcia
+4 ^ 2    # pot?ncia
 ## [1] 16
-5 %% 3   # resto da divisão de 5 por 3
+5 %% 3   # resto da divis?o de 5 por 3
 ## [1] 2
-5 %/% 3  # parte inteira da divisão de 5 por 3
+5 %/% 3  # parte inteira da divis?o de 5 por 3
 ## [1] 1
 
 
-# 2.2 - OPERADORES MATEMÁTICOS COM O R
+# 2.2 - OPERADORES MATEM?TICOS COM O R
 
 sqrt(9)   # Raiz Quadrada
 #[1] 3
-abs( - 1 )  # Módulo ou valor absoluto
+abs( - 1 )  # M?dulo ou valor absoluto
 #[1] 1
 abs( 1 )
 #[1] 1
@@ -46,7 +46,7 @@ log( 10 )   # Logaritmo natural ou neperiano (ln)
 #[1] 2.302585
 log( 10, base = 10) # Logaritmo base 10
 #[1] 1
-log10(10)   # Também logaritmo de base 10
+log10(10)   # Tamb?m logaritmo de base 10
 #[1] 1
 log( 10, base = 3.4076) # Logaritmo base 3.4076
 #[1] 1.878116
@@ -54,11 +54,11 @@ exp( 1 )       # Exponencial
 #[1] 2.718282
 factorial( 4 )         # Fatorial de 4
 #[1] 24
-choose(10, 3)          # Coeficientes binomiais: combinação de 10 3-a-3
+choose(10, 3)          # Coeficientes binomiais: combina??o de 10 3-a-3
 #[1] 120
 
 
-# 2.3 - FUNÇÕES TRIGONOMÉTRICAS
+# 2.3 - FUN??ES TRIGONOM?TRICAS
 
 sin(0.5*pi)    # Seno
 #[1] 1
@@ -79,7 +79,7 @@ atan(0)    # Arco tangente (em radianos)
 atan(0) / pi * 180
 #[1] 0
 
-# 2.4 - FUNÇÕES DE ARREDONDAMENTO
+# 2.4 - FUN??ES DE ARREDONDAMENTO
 
 ceiling( 4.3478 )
 #[1] 5
@@ -114,7 +114,7 @@ x
 ## [1] "1"
 "positivo"
 ## [1] "positivo"
-"Error: objeto x não encontrado"
+"Error: objeto x n?o encontrado"
 ## [1] "Error: objeto x n?o encontrado"
 
 # numeric
@@ -135,7 +135,7 @@ pi
 10L
 ## [1] 10
 
-# complex (raramente utilizado para análise de dados)
+# complex (raramente utilizado para an?lise de dados)
 2 + 5i
 ## [1] 2+5i
 
@@ -173,23 +173,23 @@ y <- c(1.7, "a")  ## character
 
 #character > complex > numeric > integer > logical
 
-# 3.3 - SEQUÊNCIAS NUMÉRICAS
+# 3.3 - SEQU?NCIAS NUM?RICAS
 
-# SEQUÊNCIA SIMPLES
+# SEQU?NCIA SIMPLES
 
 1:4 # CRESCENTE
 
 4:1 # DECRESCENTE
 
-# SEQUÊNCIA COM ORDEM DEFINIDA
+# SEQU?NCIA COM ORDEM DEFINIDA
 
 seq(1,20,by=2) # CRESCENTE
  
 seq(20,1,by=-2) # DECRESCENTE
 
-seq(1,20,length=5) # QUANDO APENAS QUEREMOS UM VETOR DE TAMANHO DEFINIDO, EMBORA NÃO SAIBAMOS O ESPAÇO ENTRE OS NÚMEROS
+seq(1,20,length=5) # QUANDO APENAS QUEREMOS UM VETOR DE TAMANHO DEFINIDO, EMBORA N?O SAIBAMOS O ESPA?O ENTRE OS N?MEROS
 
-# POR REPETIÇÃO
+# POR REPETI??O
 
 rep(3,times=5)
 
@@ -197,21 +197,21 @@ rep(1:5, 3)
 
 rep(1:5,each=3)
 
-# SEQUÊNCIA DE NÚMEROS EM UMA DISTRIBUIÇÃO NORMAL
+# SEQU?NCIA DE N?MEROS EM UMA DISTRIBUI??O NORMAL
 
 vn1 <-  rnorm( 1000, mean = 40,  sd = 9 )
 
-# OUTRAS DISTRIBUIÇÕES:
+# OUTRAS DISTRIBUI??ES:
 # rpois, rbinom, runif....
 
-# ORDENANDO A SEQUÊNCIA:
+# ORDENANDO A SEQU?NCIA:
 
 x  <-  sort( rnorm(1000, mean=1) ) # CRESCENTE
 
 x  <-  sort( rnorm(1000, mean=1), decreasing = T ) # DECRECENTE
 x  <-  rev(sort( rnorm(1000, mean=1) )) # DECRESCENTE
 
-# MATEMÁTICA COM VETORES
+# MATEM?TICA COM VETORES
 
 a <-  c(1, 10, 3.4, pi, pi/4, exp(-1), log( 2.23 ), sin(pi/7) )
 b <-  1:8
@@ -220,30 +220,30 @@ d <-  2.5:10
  
 sqrt( a )# RAIZ QUADRADA DE UM VETOR
 
-a - b # SOMA OU SUBTRAÇÃO DE VETORES
+a - b # SOMA OU SUBTRA??O DE VETORES
 
 a^(1/b) # POTENCIA DE VETORES
 
-mean( a ) # MÉDIA
+mean( a ) # M?DIA
 
-var( b ) # VARIÂNCIA
+var( b ) # VARI?NCIA
 
-max( c ) # VALOR MÁXIMO DENTRO DO VETOR
+max( c ) # VALOR M?XIMO DENTRO DO VETOR
 
-min( b ) # VALOR MÍNIMO DENTRO DO VETOR
+min( b ) # VALOR M?NIMO DENTRO DO VETOR
 
-sd( a ) # DESVIO PADRÃO
+sd( a ) # DESVIO PADR?O
 
 sum( c ) # SOMA DOS VALORES DENTRO DE UM VETOR
 
-range( c ) # EXTREMOS MÍNIMOS E MÁXIMOS DENTRO DE UM VETOR
+range( c ) # EXTREMOS M?NIMOS E M?XIMOS DENTRO DE UM VETOR
 
-cumsum(a) # SOMATÓRIO ACUMULATIVO DOS ELEMENTOS DO VETOR
+cumsum(a) # SOMAT?RIO ACUMULATIVO DOS ELEMENTOS DO VETOR
 
-diff(a) # DIFERENÇA ENTRE OS ELEMENTOS DO VETOR
+diff(a) # DIFEREN?A ENTRE OS ELEMENTOS DO VETOR
 
 # 3.3 - MATRIZES
-# SÃO VETORES DE 2 DIMENSÕES QUE ACEITAM APENAS ELEMENTOS DE UMA CLASSE
+# S?O VETORES DE 2 DIMENS?ES QUE ACEITAM APENAS ELEMENTOS DE UMA CLASSE
 
 m <- matrix(1:6, nrow = 2, ncol = 3)
 m
@@ -252,10 +252,10 @@ m[3,  ]   # seleciona a terceira linha
 m[ , 2]   # seleciona a segunda coluna
 m[1, 2]   # seleciona o primeiro elemento da segunda coluna
 t(m)      # matriz transposta
-m %*% n   # multiplicação matricial
+m %*% n   # multiplica??o matricial
 solve(m)  # matriz inversa de m
 
-# CRIANDO UMA MATRIZ ALEATÓRIA (INCIDÊNCIA DE UMA DOENÇA POR TRATAMENTO)
+# CRIANDO UMA MATRIZ ALEAT?RIA (INCID?NCIA DE UMA DOEN?A POR TRATAMENTO)
 m1 <- matrix( 
  rnorm(20,50,10), 
   ncol = 2,
@@ -271,24 +271,27 @@ x <- list(1:5, "Z", TRUE, c("a", "b"))
 x
 
 # 3.5 - DATA.FRAME
-# SÃO TABELAS COM DADOS A SEREM TRABALHADOS, CONTENDO ELEMENTOS DE MULTIPLAS CLASSES
+# S?O TABELAS COM DADOS A SEREM TRABALHADOS, CONTENDO ELEMENTOS DE MULTIPLAS CLASSES
 
 # 4 - IMPORTANDANDO DADOS ----
 
 tb1 <- read.csv('piraclim.csv',dec = ',')
 
-# 4.1 - COMANDOS SUPER ÚTEIS
+# 4.1 - COMANDOS SUPER ?TEIS
 
 head(tb1) # Mostra as primeiras 6 linhas.
-tail(tb1) # Mostra as últimas 6 linhas.
-nrow(tb1) # Número de linhas
-ncol(tb1) # Número de colunas
-dim(tb1) # Número de linhas e de colunas.
-names(tb1) # Os nomes das colunas (variáveis).
+tail(tb1) # Mostra as ?ltimas 6 linhas.
+nrow(tb1) # N?mero de linhas
+ncol(tb1) # N?mero de colunas
+dim(tb1) # N?mero de linhas e de colunas.
+names(tb1) # Os nomes das colunas (vari?veis).
 str(tb1) # Estrutura do data.frame. Mostra, entre outras coisas, as classes de cada coluna.
 
-# 4.2 - MUDANÇA DE CLASSES DE UMA COLUNA:
-  #CASO ESPECIAL PARA FATORES:
+# 4.2 - MUDAN?A DE CLASSES DE UMA COLUNA:
+
+
+
+#CASO ESPECIAL PARA FATORES:
 
 
 
@@ -296,7 +299,7 @@ str(tb1) # Estrutura do data.frame. Mostra, entre outras coisas, as classes de c
 
 # 5 - TRABALHANDO EM UM BANCO DE DADOS ----
 
-# 5.1 OPERADORES LÓGICOS
+# 5.1 OPERADORES L?GICOS
 
 x == y # x igual a y
 x != y # x diferente de y
@@ -305,15 +308,15 @@ x <= y #	x menor ou igual a y
 x > y #	x maior que y
 x >= y # x maior ou igual a y
 !x 	# Negativa de x
-x | y #	x ou y são verdadeiros
-x & y #	x e y são verdadeiros
-xor(x, y) #	x ou y são verdadeiros (apenas um deles)
-x %in% y - x em y
+x | y #	x ou y s?o verdadeiros
+x & y #	x e y s?o verdadeiros
+xor(x, y) #	x ou y s?o verdadeiros (apenas um deles)
+x %in% y # x em y
 
-# 5.2 - FILTRANDO DADOS POR UMA VARIÁVEL
+# 5.2 - FILTRANDO DADOS POR UMA VARI?VEL
 tb1 <- tb1[tb1$NOME!=' ',]
-# 5.3 - EXECUTANDO OPERAÇÕES MATEMÁTICAS
-# DESCOBRIR O TAMANHO DA ÁREA DE LATOSSOLO VERMELHO NO BRASIL
+# 5.3 - EXECUTANDO OPERA??ES MATEM?TICAS
+# DESCOBRIR O TAMANHO DA ?REA DE LATOSSOLO VERMELHO NO BRASIL
 unique(tb2$DESC_)
 
 # CONTINUAR A PARTIR DAQUI
@@ -339,26 +342,44 @@ if(x %in% tb1$ANO){
 
 
 # 5.5.2 - FOR (OU ESTRUTURA DE REPETI??O)
-# SEPARANDO UM NÚMERO LIMITADO DE OCORRÊNCIAS
+# SEPARANDO UM N?MERO LIMITADO DE OCORR?NCIAS
 
-df <- c()
+#ENCONTRANDO TEMPERATURA MÉDIA
+TMED <- c()
 for(i in 1:nrow(tb1)){
-  d <- tb1[tb1$RAIN>=tb1$GEOCOD[i],]
-  df <- rbind(df,d)
+  
 }
 
+# EXERCICIO PARA TROCAR A CLASSE DAS VARIAVEIS PRINCIPAIS
+
+
+
+# EXERCÍCIO PARA CORRIGIR A EVAP
+
+sample(rnorm(1000,mean=5,sd=1),1)
+
+# EXTRAINDO A MEDIA DE TODAS AS VARIAVEIS
+
+#t <- tb1[tb1$ANO %in% c(1986:1990),] # CASO QUEIRA UTILIZAR UMA FAIXA ESPECIFICA DE ANOS
+t <- tb1
+m <- c();
+for(i in 4:ncol(t)){
+  m <- append(m,mean(t[,i]))
+}
+
+names(m) <- names(t)[4:ncol(t)]
 
 # 5.6 - EXPORTANDO DADOS
 
 
-write.csv(df,'SoloSelect.csv',quote = F,dec=',',sep = ';',row.names = F)
+write.csv(df,'NovaTabela.csv',quote = F,dec=',',sep = ';',row.names = F)
 
-# OUTROS COMANDOS DE EXPORTAÇÃO
+# OUTROS COMANDOS DE EXPORTA??O
 # write.table() - PARA DADOS NO FORMATO TEXTO, OU EM OUTROS FORMATOS UTILIZADOS POR OUTROS PROGRAMAS
 # write.xlsx() - UTILIZADO PELO PACOTE 'xlsx', PARA EXPORTAR ARQUIVOS NO FORMATO EXCEL
-# Write.XLS() - FAZ O MESMO DO ACIMA, MAS DE FORMA MAIS SEGURA E PRÁTICA - PACOTE 'WriteXLS'
+# Write.XLS() - FAZ O MESMO DO ACIMA, MAS DE FORMA MAIS SEGURA E PR?TICA - PACOTE 'WriteXLS'
 
-# 6 - GRÁFICOS ----
+# 6 - GR?FICOS ----
 # 6.1 - SCATTER PLOT
 n <- 100
 x <- 1:n
@@ -378,9 +399,50 @@ hist(rnorm(1000), breaks = 6)
 # USAR EXEMPLO DO ?HELP DESSA DESTE COMANDO
 
 # 7 - FUNÇÕES ----
-# CRIE SEU PRÓPRIO COMANDO NO R
+# 7.1 CRIE SEU PRÓPRIO COMANDO NO R
 
-tp1 <- function(){
- 
+
+Estat_clim <- function(DataF,estat){
+  t <- DataF[,c(4:ncol(DataF))]
+  n <- length(t)
+  
+  resultado <- numeric(n)
+  
+  names(resultado) <- names(t)
+  
+  for(i in 1:ncol(t)){
+    resultado[i] <- estat(as.numeric(as.character(t[[i]])))
+  }
+  
+  return(resultado)
 }
+
+
+Estat_clim(tb1,mean)
+
+# 7.2 FAMILIA apply ----
+
+# apply:
+
+apply(tb1[,c(4:ncol(tb1))], 2,mean)
+
+# sapply: RESULTADO NA FORMA DE VETOR
+
+tb1[,c(4:ncol(tb1))] <- sapply(tb1[,c(4:ncol(tb1))],as.character)
+tb1[,c(4:ncol(tb1))] <- sapply(tb1[,c(4:ncol(tb1))],as.numeric)
+
+
+sapply(tb1[,c(4:ncol(tb1))], mean)
+
+# lapply: RESULTADO NA FORMA DE LISTA
+lapply(tb1[,c(4:ncol(tb1))], mean)
+
+
+# COMPARATIVO DE PERFORMANCE
+
+install.packages('microbenchmark') # CASO JA TENHA ESSA PACOTE, PULAR ESSA LINHA
+
+library(microbenchmark)
+
+microbenchmark(sapply(tb1[,c(4:ncol(tb1))], mean),tp1(tb1,mean))
 
